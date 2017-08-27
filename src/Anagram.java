97489dc817a1d821eff3cb974c93e07b03cfd77b
kfadilla <31247669+kfadilla@users.noreply.github.com>
2017-08-27 15:41:04 -0400

    Final

--- src/Anagram.java
+++ src/Anagram.java
@@ -1,7 +1,23 @@
 public class Anagram {
 	
     public static boolean anagram(String s1, String s2) {
-        // student's implementation of anagram
-        return false;
-    }
+        char[] a1 = s1.toCharArray();
+        char[] a2 = s2.toCharArray();
+        boolean TF = false;
+        if (a1.length != a2.length){
+            return false;
+        }
+        else
+            for (int i = 0; i != a1.length;++i){
+                for (char c : a1){
+                    if (c == a2[i]){
+                        TF = true;
+                        break;
+                    }
+                    else {
+                        TF = false;
+                    }
+                }
+            }
+        return TF;
 }
